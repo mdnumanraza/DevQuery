fc3ee644f7a997a7796af643678b2ff0df9d4824
Numan71417 <89739573+Numan71417@users.noreply.github.com>
2023-11-13 10:42:13 +0530

    api bug fixes

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use('/',(req, res) => {
+app.get('/',(req, res) => {
     res.send("This is a stack overflow clone API , <h1> go to <i> /questions/get </i> to get all questions </h1>")
 })
 
