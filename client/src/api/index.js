9ceec33fc332616f2c71fc902acfcb1c312da1f7
Numan71417 <89739573+Numan71417@users.noreply.github.com>
2023-11-13 10:40:39 +0530

    deploy env

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/",
+  // baseURL: "http://localhost:5000/",
+  baseURL: "https://stack-overflow-clone-tau.vercel.app/",
 });
 
 API.interceptors.request.use((req) => {
