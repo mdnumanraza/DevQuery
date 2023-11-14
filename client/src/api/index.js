77e0e674233544649a0a50205d61870ab2a853b1
Numan71417 <89739573+Numan71417@users.noreply.github.com>
2023-11-14 11:25:38 +0530

    images

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/",
-  // baseURL: "https://stack-overflow-clone-tau.vercel.app/",
+  // baseURL: "http://localhost:5000/",
+  baseURL: "https://stack-overflow-clone-tau.vercel.app/",
 });
 
 API.interceptors.request.use((req) => {
