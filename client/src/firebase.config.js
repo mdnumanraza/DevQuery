b7d8e228b007ffa81d77f69623c769344a838e9f
Numan71417 <89739573+Numan71417@users.noreply.github.com>
2023-12-05 11:09:30 +0530

    Updated env

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -1,13 +1,11 @@
-import firebase from 'firebase/compat/app'
-
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCoWokN-p9DvgOFo4DpP-XekAWaRfI6H0E",
-    authDomain: "add-images-b4898.firebaseapp.com",
-    projectId: "add-images-b4898",
-    storageBucket: "add-images-b4898.appspot.com",
-    messagingSenderId: "413471246654",
-    appId: "1:413471246654:web:581ba7a3b85195cdbb446f"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
   };
   
   // Initialize Firebase
