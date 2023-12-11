5bc531881dcd35f800ac8ce67c7a719bcbffba9f
Numan71417 <89739573+Numan71417@users.noreply.github.com>
2023-12-11 18:26:17 +0530

    final test1

--- api/index.js
+++ api/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
 app.use(
     cors({
-      origin: "http://localhost:3000", 
+      origin: ["https://numan-stackoverflow.vercel.app/","http://localhost:3000"] ,
       credentials: true,
     })
   );
