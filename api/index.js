a96f1b5e12f1fb97b7b16e3a7950f3e2c3824038
Mohammed Numan Raza <89739573+Numan71417@users.noreply.github.com>
2024-01-13 10:35:14 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }));
 
 app.use(
     cors({
-      origin: ["https://numan-stackoverflow.vercel.app/","http://localhost:3000"] ,
+      origin: ["https://numan-stackoverflow.vercel.app","http://localhost:3000","*"] ,
       methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
       credentials: true,
     })
