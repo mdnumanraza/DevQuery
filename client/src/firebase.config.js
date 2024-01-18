e36d94d930447c8df360ed451035416c4235ccf8
Mohammed Numan Raza <89739573+Numan71417@users.noreply.github.com>
2024-01-18 15:10:14 +0530

    Update firebase.config.js

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -7,14 +7,15 @@
 //     appId: process.env.REACT_APP_APP_ID, 
 //   }
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCoWokN-p9DvgOFo4DpP-XekAWaRfI6H0E",
+const firebaseConfig = {
+    apiKey: "AIzaSyCoWokN-p9DvgOFo4DpP-XekAWaRfI6H0E",
   authDomain: "add-images-b4898.firebaseapp.com",
   projectId: "add-images-b4898",
   storageBucket: "add-images-b4898.appspot.com",
   messagingSenderId: "413471246654",
   appId: "1:413471246654:web:581ba7a3b85195cdbb446f"
-})
+  }
+
 
 
 export default firebaseConfig
